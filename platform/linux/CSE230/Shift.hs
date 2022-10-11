90c7c9033383fabb024b88ade8e0945e85ef24ef
mkolosick <matt@kolosick.com>
2022-10-11 14:23:46 -0700

    removed unused platform functions

--- platform/linux/CSE230/Shift.hs
+++
@@ -1,9 +0,0 @@
-module CSE230.Shift where 
-
-import qualified Graphics.Gloss as G
-
-shift :: Float -> (Float, Float) -> (Float, Float)
-shift _ (x, y) = (x, y)
-
-scale :: G.Picture -> G.Picture
-scale = id
